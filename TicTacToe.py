d44bfab50986ac7ed43299723d462fd9af859a5f
Rishi Soni <rishisoni.5678@gmail.com>
2020-04-14 21:17:40 -0400

    Update to cosmetics and opponent

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,20 +1,24 @@
-import random
 import time
+from copy import deepcopy
 
-game = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+game = [["-", "-", "-"],
+        ["-", "-", "-"],
+        ["-", "-", "-"]]
 
-def show_board(b):
-    for i in b:
-        print(i)
-    print("\n")
+def show_board():
+    s = ""
+    for i in game:
+        line = " | ".join([str(e) for e in i])
+        line += "\n"
+        s += line
+    print(s)
 
-def open_spots(b):
+
+def open_spots(board):
     spots = []
     for i in range(3):
         for i2 in range(3):
-            if b[i][i2] == 0:
+            if board[i][i2] == "-":
                 spots.append(i * 3 + i2)
     return spots
 
@@ -22,41 +26,39 @@ def player_move():
     move = int(input("Player move: "))
     while move not in open_spots(game):
         move = int(input("Try Again: "))
-    game[move // 3][move % 3] = 1
+    game[move // 3][move % 3] = "X"
 
-def find_computer_move():
-    copy = game
-    #Testing for possible win
-    for i in open_spots(game):
-        copy[i // 3][i % 3] = 2
-        if not possible_win(copy):
-            copy[i // 3][i % 3] = 0
-            return i
-        else:
-            copy[i // 3][i % 3] = 0
-    #Testing for blocking player win
-    for i in open_spots(game):
-        copy[i // 3][i % 3] = 1
-        if not possible_win(copy):
-            copy[i // 3][i % 3] = 0
-            return i
-        else:
-            copy[i // 3][i % 3] = 0
-    #Place in center
-    if game[1][1] == 0:
-        return 4
-    #Place in corners
-    for i in [0, 2, 6, 8]:
-        if game[i // 3][i % 3] == 0:
-            return i
-    return random.choice(open_spots(game))
+def minimax(board, is_maximizing):
+    evaluate_board = possible_end(board)
+    if evaluate_board[0]:
+        return [evaluate_board[1], ""]
+    best_move = 0
+    if is_maximizing:
+        best_value = -float("Inf")
+        for move in open_spots(board):
+            new_board = deepcopy(board)
+            new_board[move // 3][move % 3] = "X"
+            hypothetical_value = minimax(new_board, not is_maximizing)[0]
+            if hypothetical_value > best_value:
+                best_value = hypothetical_value
+                best_move = move
+    else:
+        best_value = float("Inf")
+        for move in open_spots(board):
+            new_board = deepcopy(board)
+            new_board[move // 3][move % 3] = "O"
+            hypothetical_value = minimax(new_board, not is_maximizing)[0]
+            if hypothetical_value < best_value:
+                best_value = hypothetical_value
+                best_move = move
+    return [best_value, best_move]
 
 def computer_move():
-    move = find_computer_move()
-    game[move // 3][move % 3] = 2
+    move = minimax(game, False)[1]
+    game[move // 3][move % 3] = "O"
     print(f"The computer played a move at {move}.")
 
-def possible_win(board):
+def possible_end(board):
     wins = [
         [board[0][0], board[0][1], board[0][2]],
         [board[1][0], board[1][1], board[1][2]],
@@ -67,56 +69,50 @@ def possible_win(board):
         [board[0][0], board[1][1], board[2][2]],
         [board[2][0], board[1][1], board[0][2]],
     ]
-    if [1] * 3 in wins or [2] * 3 in wins or open_spots(board) == []:
-        return False
+    if ["X"] * 3 in wins:
+        return [True, 1]
+    elif ["O"] * 3 in wins:
+        return [True, -1]
+    elif not open_spots(board):
+        return [True, 0]
     else:
-        return True
+        return [False, 0]
 
-def game_over(board):
-    wins = [
-        [board[0][0], board[0][1], board[0][2]],
-        [board[1][0], board[1][1], board[1][2]],
-        [board[2][0], board[2][1], board[2][2]],
-        [board[0][0], board[1][0], board[2][0]],
-        [board[0][1], board[1][1], board[2][1]],
-        [board[0][2], board[1][2], board[2][2]],
-        [board[0][0], board[1][1], board[2][2]],
-        [board[2][0], board[1][1], board[0][2]],
-    ]
-    if [1] * 3 in wins:
+def game_over(winner):
+    if winner == 1:
         print("-----PLAYER WINS!-----")
-    elif [2] * 3 in wins:
+    elif winner == -1:
         print("-----COMPUTER WINS!-----")
     else:
         print("-----DRAW!-----")
 
 def main():
-    show_board(game)
-    while possible_win(game):
+    show_board()
+    while not possible_end(game)[0]:
         player_move()
-        show_board(game)
-        if possible_win(game):
+        show_board()
+        if not possible_end(game)[0]:
             computer_move()
-            show_board(game)
-    game_over(game)
+            show_board()
+    game_over(possible_end(game)[1])
 
 if __name__ == "__main__":
     print("-----WELCOME TO TIC TAC TOE!-----\n")
     time.sleep(2)
-    print("X's are represented with 1, O's are represented with 2, and blank spaces are represented by 0.")
-    time.sleep(4)
-    print("Input a number from 0-8 to place objects on the board.")
-    print("[0, 1, 2]")
-    print("[3, 4, 5]")
-    print("[6, 7, 8]\n")
-    time.sleep(4)
+    print("Blank spaces are represented by dashes.")
+    time.sleep(2)
+    print("Use keys 0-8 to place objects on the board.")
+    print("0 | 1 | 2")
+    print("3 | 4 | 5")
+    print("6 | 7 | 8\n")
+    time.sleep(3)
     main()
     response = input("Play Again? (y/n) ")
     print("\n")
     while response == "y":
-        game = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]]
+        game = [["-", "-", "-"],
+                ["-", "-", "-"],
+                ["-", "-", "-"]]
         main()
         response = input("Play Again? (y/n) ")
         print("\n")
